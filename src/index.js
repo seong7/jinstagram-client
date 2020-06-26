4fc984e1b46b924884a6e0b1de4232b401f6fc35
Seongjin <seongjinkim1123@gmail.com>
2020-06-26 17:06:52 +0900

    Add and Set Redux

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
+import { composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './modules';
 import 'normalize.css';
 import './global.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
