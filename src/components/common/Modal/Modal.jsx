98b2386a51a69e451be7c4aa12f5e8b936e4f030
Seongjin <seongjinkim1123@gmail.com>
2020-06-28 17:30:59 +0900

    Create component - Modal

---
+++ src/components/common/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div className='modal__wrapper'>
+      <div className='modal__content'></div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,6 @@
 import Button from './Button/Button.jsx';
 import Form from './Form/Form.jsx';
 import Input from './Input/Input.jsx';
+import Modal from './Modal/Modal.jsx';
 
-export { Button, Form, Input };
+export { Button, Form, Input, Modal };
