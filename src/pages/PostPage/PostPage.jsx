63c208cc7ff3297095733aa1fe38f4b11e103e4c
Seongjin <seongjinkim1123@gmail.com>
2020-07-15 16:58:06 +0900

    Add PostPage, Post containter

---
+++ src/pages/PostPage/PostPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageLayout } from '../styled';
+import { Post } from '../../containers';
+
+const PostPage = () => {
+  return (
+    <PageLayout>
+      <Post />
+    </PageLayout>
+  );
+};
+
+export default PostPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import LoginPage from './LoginPage/LoginPage.jsx';
 import JoinPage from './JoinPage/JoinPage.jsx';
+import PostPage from './PostPage/PostPage.jsx';
 
-export { LoginPage, JoinPage };
+export { LoginPage, JoinPage, PostPage };
