11d3b7706cb9acbf50de7eee64855e682741d4d3
Seongjin <seongjinkim1123@gmail.com>
2020-07-21 19:08:27 +0900

    Create variables.scss for Dark mode / Light mode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 import 'normalize.css';
-import './global.css';
+import './variables.scss';
+import './index.scss';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
