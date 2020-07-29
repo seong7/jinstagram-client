53e0181f25a084ccafbdcfc2b0dde8d0dbefec1d
Seongjin <seongjinkim1123@gmail.com>
2020-07-29 16:22:51 +0900

    Fix vedoe src - Login Page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -19,14 +19,14 @@ const LoginPage = () => {
             autoPlay
             muted
             loop
-            poster={process.env.REACT_APP_HOME_VIDEO_POSTER}
+            poster='https://www.pexels.com/assets/videos/free-videos-7daa2ef41a140f70c757ce91913a4ecb90570b7d7cd2b401bae868350e02c83a.jpg'
           >
             <source
-              src={process.env.REACT_APP_HOME_VIDEO_MP4}
+              src='https://static.pexels.com/lib/videos/free-videos.mp4'
               type='video/mp4'
             />
             <source
-              src={process.env.REACT_APP_HOME_VIDEO_WEBM}
+              src='https://static.pexels.com/lib/videos/free-videos.webm'
               type='video/webm'
             />
           </video>
